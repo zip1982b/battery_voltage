5df8a7b3717f6058b52a9a00278fbd13ec912614
zip1982b <zip1982b@gmail.com>
2023-03-16 08:14:32 +0200

    initial commit

--- src/main.c
+++ src/main.c
@@ -1,88 +1,80 @@
-/*
- * Copyright (c) 2020 Libre Solar Technologies GmbH
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-
-#include <inttypes.h>
-#include <stddef.h>
-#include <stdint.h>
-
 #include <zephyr/device.h>
 #include <zephyr/devicetree.h>
 #include <zephyr/drivers/adc.h>
 #include <zephyr/kernel.h>
-#include <zephyr/sys/printk.h>
-#include <zephyr/sys/util.h>
 
-#if !DT_NODE_EXISTS(DT_PATH(zephyr_user)) || \
-	!DT_NODE_HAS_PROP(DT_PATH(zephyr_user), io_channels)
-#error "No suitable devicetree overlay specified"
+#define ADC_NODE 	DT_NODELABEL(adc)
+static const struct device *adc_dev = DEVICE_DT_GET(ADC_NODE);
+
+#define ADC_RESOLUTION 10							//0-1023
+#define ADC_CHANNEL		0
+#define ADC_PORT SAADC_CH_PSELP_PSELP_AnalogInput0 //AIN0
+#define ADC_REFERENCE ADC_REF_INTERNAL				//0.6V
+#define ADC_GAIN ADC_GAIN_1_5						//ADC_REFERENCE*5
+
+struct adc_channel_cfg chnl0_cfg = {
+	.gain = ADC_GAIN,
+	.reference = ADC_REFERENCE,
+	.acquisition_time = ADC_ACQ_TIME_DEFAULT,
+	.channel_id = ADC_CHANNEL,
+#ifdef CONFIG_ADC_NRFX_SAADC
+	.input_positive = ADC_PORT
 #endif
-
-#define DT_SPEC_AND_COMMA(node_id, prop, idx) \
-	ADC_DT_SPEC_GET_BY_IDX(node_id, idx),
-
-/* Data of ADC io-channels specified in devicetree. */
-static const struct adc_dt_spec adc_channels[] = {
-	DT_FOREACH_PROP_ELEM(DT_PATH(zephyr_user), io_channels,
-			     DT_SPEC_AND_COMMA)
 };
+
+
+int16_t sample_buffer[1];
+struct adc_sequence sequence = {
+	.channels = BIT(ADC_CHANNEL),
+	.buffer = sample_buffer,
+	.buffer_size = sizeof(sample_buffer),
+	.resolution = ADC_RESOLUTION
+};
+
 
 void main(void)
 {
 	int err;
-	int16_t buf;
-	struct adc_sequence sequence = {
-		.buffer = &buf,
-		/* buffer size in bytes, not number of samples */
-		.buffer_size = sizeof(buf),
-	};
 
-	/* Configure channels individually prior to sampling. */
-	for (size_t i = 0U; i < ARRAY_SIZE(adc_channels); i++) {
-		if (!device_is_ready(adc_channels[i].dev)) {
-			printk("ADC controller device not ready\n");
-			return;
-		}
-
-		err = adc_channel_setup_dt(&adc_channels[i]);
-		if (err < 0) {
-			printk("Could not setup channel #%d (%d)\n", i, err);
-			return;
-		}
+	if (!device_is_ready(adc_dev)) {
+		printk("ADC controller device not ready\n");
+		return;
 	}
+
+	err = adc_channel_setup(adc_dev, &chnl0_cfg);
+	if (err < 0) {
+		printk("Could not setup channel #%d\n", err);
+		return;
+	}
+	
 
 	while (1) {
 		printk("ADC reading:\n");
-		for (size_t i = 0U; i < ARRAY_SIZE(adc_channels); i++) {
-			int32_t val_mv;
+		err = adc_read(adc_dev, &sequence);
+		if (err != 0) {
+			printk("Could not read (%d)\n", err);
+			continue;
+		} 
+		else {
+			printk("sample buffer = %d\n", sample_buffer);
+		}
 
-			printk("- %s, channel %d: ",
-			       adc_channels[i].dev->name,
-			       adc_channels[i].channel_id);
 
-			(void)adc_sequence_init_dt(&adc_channels[i], &sequence);
 
-			err = adc_read(adc_channels[i].dev, &sequence);
-			if (err < 0) {
-				printk("Could not read (%d)\n", err);
-				continue;
-			} else {
-				printk("%"PRId16, buf);
-			}
+		/* conversion to mV may not be supported, skip if not */
+		int32_t val_mv = sample_buffer[0];
 
-			/* conversion to mV may not be supported, skip if not */
-			val_mv = buf;
-			err = adc_raw_to_millivolts_dt(&adc_channels[i],
-						       &val_mv);
-			if (err < 0) {
+		printk("ADC value = %d\n", val_mv);
+		
+		int32_t adc_vref = adc_ref_internal(adc_dev);
+
+		err = adc_raw_to_millivolts(adc_vref, ADC_GAIN, ADC_RESOLUTION, &val_mv);
+		if (err < 0) {
 				printk(" (value in mV not available)\n");
 			} else {
-				printk(" = %"PRId32" mV\n", val_mv);
+				printk("ADC voltage = %d mV\n", val_mv);
 			}
 		}
 
-		k_sleep(K_MSEC(1000));
-	}
+		k_sleep(K_MSEC(10000));
 }
